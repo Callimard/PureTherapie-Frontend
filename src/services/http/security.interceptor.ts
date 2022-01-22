ef7e7960427def03176661c3c057b1bbba2c09a1
Guillaume Rakotomalala <guillaume.rakotomalala@sealandblock.fr>
2022-01-22 21:38:40 +0100

    #1ve7auu[in progress] Client Administration in implementation

--- src/services/http/security.interceptor.ts
+++ src/services/http/security.interceptor.ts
@@ -3,16 +3,23 @@ import {HttpEvent, HttpHandler, HttpInterceptor, HttpRequest} from '@angular/com
 import {Observable} from 'rxjs';
 import {CookieService} from "ngx-cookie-service";
 import {GlobalVariables} from "../../global/global-variables";
+import {AuthenticationService} from "../auth/authentication.service";
 
 @Injectable({
   providedIn: 'root'
 })
 export class SecurityInterceptor implements HttpInterceptor {
 
-  constructor(private cookieService: CookieService) {
+  constructor(private cookieService: CookieService, private authenticationService: AuthenticationService) {
   }
 
   intercept(request: HttpRequest<unknown>, next: HttpHandler): Observable<HttpEvent<unknown>> {
+
+    if (this.authenticationService.isAuthenticated()) {
+      request = request.clone({
+        withCredentials: true
+      });
+    }
 
     if (this.cookieService.check(GlobalVariables.CSRF_COOKIE) && !request.withCredentials) {
       request = request.clone({
