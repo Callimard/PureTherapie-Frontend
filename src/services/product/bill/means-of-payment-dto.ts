4a5d22b262d1d075cde117ae1e8cff191b8cb464
Guillaume Rakotomalala <guillaume.rakotomalala@sealandblock.fr>
2022-02-13 11:46:01 +0100

    #1xy89cq[in progress] Groupon payment are now accepted

--- src/services/product/bill/means-of-payment-dto.ts
+++ src/services/product/bill/means-of-payment-dto.ts
@@ -1,3 +1,5 @@
+import {GlobalVariables} from "../../../global/global-variables";
+
 export class MeansOfPaymentDTO {
 
   constructor(public idMeansOfPayment: number,
@@ -7,6 +9,10 @@ export class MeansOfPaymentDTO {
 
   public static default() {
     return new MeansOfPaymentDTO(-1, "DEFAULT_MEANS_OF_PAYMENT", "DEFAULT_MEANS_OF_PAYMENT");
+  }
+
+  public static isGrouponPayment(meansOfPayment: MeansOfPaymentDTO): boolean {
+    return meansOfPayment.name === GlobalVariables.GROUPON_PAYMENT;
   }
 
 }
--- src/tool/bill-tool.ts
+++ src/tool/bill-tool.ts
@@ -1,4 +1,5 @@
 import {BillDTO} from "../services/product/bill/bill-dto";
+import {MeansOfPaymentDTO} from "../services/product/bill/means-of-payment-dto";
 
 export class BillTool {
 
@@ -25,8 +26,13 @@ export class BillTool {
   public static remainingBalance(bill: BillDTO): number {
     let amountPaid = 0.0;
     for (let payment of bill.payments) {
-      if (!payment.canceled)
-        amountPaid += payment.amountPaid;
+      if (!payment.canceled) {
+        if (MeansOfPaymentDTO.isGrouponPayment(payment.meansOfPayment)) {
+          return 0.0
+        } else {
+          amountPaid += payment.amountPaid;
+        }
+      }
     }
 
     return bill.purchasePrice - amountPaid;
