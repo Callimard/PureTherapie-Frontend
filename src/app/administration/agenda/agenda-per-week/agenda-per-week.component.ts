980d51e8929e689df8dd990c93f983401e4f070d
Guillaume Rakotomalala <guillaume.rakotomalala@sealandblock.fr>
2022-03-07 10:48:00 +0100

    #22k4u76[in progress] Clean du code de l'agenda pour préparer l'ajout des nouvelles vues.

---
+++ src/app/administration/agenda/agenda-per-week/agenda-per-week.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-agenda-per-week',
+  templateUrl: './agenda-per-week.component.html',
+  styleUrls: ['./agenda-per-week.component.css']
+})
+export class AgendaPerWeekComponent implements OnInit {
+
+  constructor() {
+    // Normal
+  }
+
+  ngOnInit(): void {
+    // Normal
+  }
+
+}
